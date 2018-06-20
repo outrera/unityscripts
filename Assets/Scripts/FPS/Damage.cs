c16ed832ff00f6974a1d7aa62ec5eb50b1188842
Jeremy Bullock <jcbullock@gmail.com>
2018-06-20 08:43:19 -0600

    Fix Fall Damage

--- Assets/Scripts/FPS/Damage.cs
+++ Assets/Scripts/FPS/Damage.cs
@@ -64,6 +64,7 @@ public class Damage : MonoBehaviour {
                     if (takeFallDamage)
                     {
                         damage = true;
+                        takeFallDamage = false;
                     }
                 }
                 else
