13d8b3771a639c75493824840434cc642fe05a9a
Jeremy Bullock <jcbullock@gmail.com>
2018-06-21 13:21:44 -0600

    Added Enabler Script

---
+++ Assets/Scripts/Misc/EnterEnable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterEnable : MonoBehaviour {
+
+	public GameObject objectToEnable;
+	public bool collectAll;
+	// Use this for initialization
+	void Start () {
+		objectToEnable.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.CompareTag("Player")) {
+			if (collectAll) {
+				GameObject[] collected = GameObject.FindGameObjectsWithTag ("Collect");
+				if (collected.Length > 0) {	
+					objectToEnable.SetActive(true);	
+				}
+			}
+			else objectToEnable.SetActive(true);
+		}
+	}
+}
