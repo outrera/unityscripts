8ab33dad8f19fd81a7b35d57c80b96eadc4ba6aa
Jeremy Bullock <jeremy.bullock@asd20.org>
2018-07-25 10:52:14 -0600

    Added Start Text Script

---
+++ Assets/Scripts/Misc/StartText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartText : MonoBehaviour {
+    [Header("Start Text")]
+    [Tooltip("Shows Text on Screen for x seconds")]
+    public int secondsToShow = 5;
+    public Text startText;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        startText.CrossFadeAlpha(0.0f, secondsToShow, false);
+	}
+}
